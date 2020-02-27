1cdee4d86a6642419a333eb66fae5d94c5ee39cb
gomsang <gyeongrok.kim@gomsang.com>
2020-02-27 22:06:32 +0900

    doit 3_8 solved.

---
+++ doit_problems/doit_3_8.cpp
@@ -0,0 +1,48 @@
+//
+// Created by gomsang on 2/27/2020.
+//
+
+#include <stdio.h>
+#include <iostream>
+
+void *binsearch(const void *key, const void *base, size_t nmemb, size_t size, int(*compar)(const void *, const void *)) {
+    int left = 0;
+    int right = (int) nmemb;
+    do {
+        int center = (left + right) / 2;
+        int com = compar((int *) base + center, key);
+
+        if (com == 0) {
+            return (int *) base + center;
+        } else if (com < 0) {
+            left = center + 1;
+        } else {
+            right = center - 1;
+        }
+    } while (left <= right);
+}
+
+int compar(const int *x, const int *y) {
+    if (*x > *y) {
+        return 1;
+    } else if (*x < *y) {
+        return -1;
+    } else {
+        return 0;
+    }
+}
+
+int main(void) {
+    int key = 2;                    // keyword
+    int base[] = {1, 2, 3, 4, 5};   // base array
+    size_t nx = 5;                  // base array's length
+    size_t size = sizeof(int);      // size of each elements in array.
+    int *p = (int *) binsearch(&key, base, nx, size, (int (*)(const void *, const void *)) compar);
+
+    if (p == NULL)
+        puts("search failed");
+    else
+        printf("%d is in x[%d]\n", key, (int) (p - base));
+
+    free(base);
+}
